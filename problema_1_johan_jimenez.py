6abf35ed0a5d837378ddf0de0ac121957ecfa18f
zSnails <erizojuan33@gmail.com>
2022-03-21 20:19:02 -0600

    Rename ExaPart1.py to problema_1_johan_jimenez.py

---
+++ problema_1_johan_jimenez.py
@@ -0,0 +1,32 @@
+fecha1 = int(input("Ingrese la primera fecha: "))
+fecha2 = int(input("Ingrese la segunda fecha: "))
+ano_fech1 = fecha1//10000
+ano_fech2 = fecha2//10000
+dia_fecha1 = fecha1%100 
+dia_fecha2 = fecha2%100
+
+i = 0
+
+while i < 3:
+    mes_fech1 = fecha1%10
+    mes_fech2 = fecha2%10
+    fecha1 = (fecha1-mes_fech1)//10
+    fecha2 = (fecha2-mes_fech2)//10
+    i = i+1
+if (mes_fech1 < mes_fech2):
+    anofinal = ano_fech2-ano_fech1
+else:
+    anofinal = (ano_fech2-ano_fech1)-1
+if (mes_fech1 > mes_fech2):
+    mesfinal = (mes_fech2+12)-mes_fech1
+elif (mes_fech1 == mes_fech2):
+    mesfinal = 0
+else: 
+    mesfinal = (mes_fech1+12)-mes_fech2
+if (dia_fecha1 > dia_fecha2):
+    diafinal = dia_fecha1-dia_fecha2
+else:
+    diafinal = dia_fecha2-dia_fecha1
+fechafinal = ((anofinal*100)+mesfinal)
+fechafinal = (fechafinal*100)+diafinal
+print(fechafinal)
